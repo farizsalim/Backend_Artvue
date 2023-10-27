a2e19e5468c7268a01c469370dc6cb5b47bd1dca
farizsalim <farizsalim10@yahoo.co.id>
2023-10-28 06:05:04 +0800

    give default route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+router.get('/', (req,res) => {
+    res.send({
+        status: 'Successfully',
+        message: 'Welcome to Art Vue Server'
+    })
+})
 router.post('/register', authController.register);
 router.get('/allUser', authController.getAllUsers)
 router.put('/user/:IDUser/updateUsername', authController.updateUsername);
