ffc37da7d87d379baffed7db10ae575e3d3ca10c
farizsalim <farizsalim10@yahoo.co.id>
2023-10-28 05:55:31 +0800

    nothing else change

--- dbconfig.js
+++ dbconfig.js
@@ -4,24 +4,24 @@ const mysql = require('mysql2')
 
 dotenv.config();
 
-
 const db = new Sequelize({
   dialect: 'mysql',
   database: process.env.DB_DATABASE,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  host: 'localhost',
+  host: process.env.DB_HOST,
   define: {
     timestamps: false,
   },
 });
 
+
 module.exports = db;
 
 /* Kalau Pakai Mysql2 */
 // const db = mysql.createConnection({
-//     host: 'localhost',       
-//     user: 'root',       
+//     host: process.env.DB_HOST,       
+//     user: process.env.DB_USER,       
 //     password: process.env.DB_PASSWORD, 
 //     database: process.env.DB_DATABASE 
 //   });   
